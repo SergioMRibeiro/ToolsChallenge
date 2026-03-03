201da2bf597f17cde35eab40d5e60f037464ca03
SergioMRibeiro <sergio.ripariu@gmail.com>
2026-03-03 12:28:23 -0300

    first commit

---
+++ src/main/java/com/toolschalange/toolschalange/ToolschalangeApplication.java
@@ -0,0 +1,13 @@
+package com.toolschalange.toolschalange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolschalangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolschalangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolschalange/toolschalange/ToolschalangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolschalange.toolschalange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolschalangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
